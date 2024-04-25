0e830a502054b2df237f7b0a98865de76049b60f
DRezeanu <DRezeanu@uw.edu>
2024-04-25 11:07:38 -0700

    Create Lab5 Python File

---
+++ Dragos_SPHSC525_Lab5.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+def makeGrating(size, sf, ori, phase, contrast):
+    x, y = np.meshgrid(np.linspace(0, size, size), np.linspace(0,size,size))
+    center_value = size//2
+
+    gradient = np.sin(np.deg2rad(ori))*x + np.cos(np.deg2rad(ori))*y
+
+    grating = np.sin((2*np.pi * gradient) / sf) + np.deg2rad(phase)
+
+    grating *= contrast
+
+    return grating
+
